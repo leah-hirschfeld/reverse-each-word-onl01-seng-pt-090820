4cf2e0f143a13d07db4b71276d206cec451e27b1
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-09-12 23:57:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -17,5 +17,5 @@ def reverse_each_word(string)
   array.collect do |i| 
     array_reversed = array_reversed << i.reverse
   end
-    returns array_reversed.join(" ")
+    return array_reversed.join(" ")
 end
